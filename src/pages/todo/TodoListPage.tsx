18287b1a3e1c0d0b8f1cd9ca5d328c8e40433073
soob <1008sb354@gmail.com>
2024-02-05 17:59:17 +0900

    feat: zustand todo 리스트 추가

--- src/pages/todo/TodoListPage.tsx
+++ src/pages/todo/TodoListPage.tsx
@@ -5,6 +5,13 @@ const TodoListPage = () => {
     <ListTable
       data={[
         {
+          key: "todo_zustand",
+          title: "zustand를 이용한 todo",
+          link: "/todo/zustand",
+          skill: ["zustand"],
+          date: "2024-02-05",
+        },
+        {
           key: "todo_redux",
           title: "리덕스 툴킷을 이용한 todo",
           link: "/todo/redux",
